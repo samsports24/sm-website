5b77bc268620a4018293c7a9e274879673aa5f0a
hamzakhandecotechs <bilalalirizvi5@gmail.com>
2023-07-22 17:56:25 +0500

    updated: league score page

--- src/pages/LeagueScore.jsx
+++ src/pages/LeagueScore.jsx
@@ -7,6 +7,9 @@ import { Button, Col, Image, Row } from 'antd'
 // Image, Icon
 import bellIcon from '../assets/bell-icon.svg'
 import circaImage from '../assets/teams/circa_sports_trout.png'
+import UfaflImage from '../assets/ufafl.png'
+
+// Component
 import LeagueScoreCard from '../components/cards/leagueScoreCard'
 
 const LeagueScore = () => {
@@ -60,31 +63,61 @@ const LeagueScore = () => {
               <Image preview={false} src={circaImage} />
             </div>
             <p>
-              League Notification <img src={bellIcon} alt='Icon' />
+              <span>League Notification</span> <img src={bellIcon} alt='Icon' />
             </p>
           </div>
           <div className='center'>
             <div className='title_box'>
               <h1>Circa Sports Trout</h1>
               <p>
-                Live Player Auction <img src={bellIcon} alt='Icon' />
+                <span>Live Player Auction</span> <img src={bellIcon} alt='Icon' />
               </p>
             </div>
-            <div className='button_box'>
-              <Button>Overall Record</Button>
-              <Button>Division Record</Button>
-            </div>
-            <div className='team_financials_box'>
-              <p>Team Financials</p>
-              <div>
-                <p>Live Player Auction</p>
-                <p>---</p>
+            <div className='button_and_team_box'>
+              <div className='button_box'>
+                <Button>Overall Record</Button>
+                <Button>Division Record</Button>
+              </div>
+              <div className='team_financials_box'>
+                <p>Team Financials</p>
+                <div>
+                  <p>Live Player Auction</p>
+                  <span>---</span>
+                </div>
+                <div>
+                  <p>Live Player Auction</p>
+                  <span>---</span>
+                </div>
               </div>
             </div>
           </div>
-          <div className='right'>right</div>
+          <div className='right'>
+            <div className='content'>
+              <div className='top'>
+                <span>23&apos;</span>
+                <p> Same year Price-Pool</p>
+              </div>
+              <div className='content2'>
+                <div className='image_div'>
+                  <Image preview={false} src={UfaflImage} alt='UFAFL' />
+                </div>
+                <div className='content3'>
+                  <div className='top'>
+                    <span>23&apos;</span>
+                    <p>Price-Pool</p>
+                  </div>
+                  <div className='top'>
+                    <span>23&apos;</span>
+                    <p>Price-Pool</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <h1>UFAFL Price_Pools</h1>
+          </div>
         </header>
         <section className='score_card_container'>
+          <h1 className='league_score_heading'>League Scores </h1>
           <Row gutter={[30, 20]}>
             {scores?.map((value, index) => (
               <Col lg={12} xl={12} xxl={8} key={index}>
