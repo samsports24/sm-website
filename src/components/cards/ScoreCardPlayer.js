d1a9f0cbb6fed8c1475d4e4d9cffd54f8f0ea42d
hamzakhandecotechs <bilalalirizvi5@gmail.com>
2023-08-02 10:13:21 +0500

    updated: game detail page responsive

--- src/components/cards/ScoreCardPlayer.js
+++ src/components/cards/ScoreCardPlayer.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Image } from 'antd'
 
 const ScoreCardPlayer = ({ alignment, data }) => {
-  console.log(alignment)
   return (
     <div className='sc-player'>
       <div
