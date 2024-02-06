13c41ada762e82ba01bf8f319012f551dd6c3f8f
hamzakhandecotechs <bilalalirizvi5@gmail.com>
2024-02-06 10:57:11 +0500

    updated: responsiveness

--- src/pages/LandingPage/FantasySportsSection.js
+++ src/pages/LandingPage/FantasySportsSection.js
@@ -4,9 +4,34 @@ import { Typography } from 'antd'
 
 const FantasySportsSection = () => {
   return (
-    <div className='fantasy_sports'>
-      <div className='content'>
-        <div className='top'>
+    <>
+      <div className='fantasy_sports'>
+        <div className='main_heading'>
+          <Typography.Title level={1}>
+            LET&apos;S DISCUSS
+            <br />
+            FANTASY SPORTS
+          </Typography.Title>
+        </div>
+        <div className='contentDesktop'>
+          <Content mainTitle />
+        </div>
+        <video src={Video} width='100%' height='100%' muted autoPlay loop controls={false} />
+      </div>
+      <div className='fantasy_sports fantasy_sports_small_screen '>
+        <div className='contentSmallScreen'>
+          <Content />
+        </div>
+      </div>
+    </>
+  )
+}
+
+const Content = ({ mainTitle }) => {
+  return (
+    <div className='content'>
+      <div className='top'>
+        {mainTitle && (
           <Typography.Title level={1}>
             LET&apos;S
             <br />
@@ -16,31 +41,29 @@ const FantasySportsSection = () => {
             <br />
             SPORTS
           </Typography.Title>
+        )}
+      </div>
+      <div className='bottom'>
+        <p className='text1'>SAMSPORTS</p>
+        <div>
+          <p className='text2'>c/ELISA 17</p>
+          <p className='text3'>08023 Barcelona, Spain</p>
         </div>
-        <div className='bottom'>
-          <p className='text1'>SAMSPORTS</p>
-          <div>
-            <p className='text2'>c/ELISA 17</p>
-            <p className='text3'>08023 Barcelona, Spain</p>
-          </div>
-          <div>
-            <p className='text4'>
-              Email: <span>support@samsports.io</span>
-            </p>
-            <p className='text5'>
-              Social: <span>@SAMSports_</span>
-            </p>
-          </div>
-          <p className='text6'>
-            SAMSPORTS is not in any way affiliated with any sports team or sports league, nor is it
-            affiliated with any player, players association, or collection of players. The use of
-            sports players’ names in conjunction with their statistics in our contests is intended
-            for informational purposes only and should not be construed as an endorsement of our
-            contests.
+        <div>
+          <p className='text4'>
+            Email: <span>support@samsports.io</span>
+          </p>
+          <p className='text5'>
+            Social: <span>@SAMSports_</span>
           </p>
         </div>
+        <p className='text6'>
+          SAMSPORTS is not in any way affiliated with any sports team or sports league, nor is it
+          affiliated with any player, players association, or collection of players. The use of
+          sports players’ names in conjunction with their statistics in our contests is intended for
+          informational purposes only and should not be construed as an endorsement of our contests.
+        </p>
       </div>
-      <video src={Video} width='100%' height='100%' muted autoPlay loop controls={false} />
     </div>
   )
 }
--- src/pages/LandingPage/Navbar.js
+++ src/pages/LandingPage/Navbar.js
@@ -1,9 +1,12 @@
 import { useState } from 'react'
 import { Button, Drawer } from 'antd'
+
 import Logo from '../../assets/Logo.svg'
 import Title from '../../assets/landing/title.png'
+import SamLogo from '../../assets/sam-football.png'
 
 import { FaBars } from 'react-icons/fa'
+import { IoClose } from 'react-icons/io5'
 
 const Navbar = () => {
   return (
@@ -27,10 +30,7 @@ const Navbar = () => {
         <Button shape='round' type='primary' className='auth_btn'>
           Login
         </Button>
-        <div className='menu_bar'>
-          <FaBars style={{ color: '#fff' }} size={22} />
-        </div>
-        {/* <DrawerMenu /> */}
+        <DrawerMenu />
       </div>
     </div>
   )
@@ -42,17 +42,53 @@ const DrawerMenu = () => {
   const showDrawer = () => setOpen(true)
   const onClose = () => setOpen(false)
 
+  const handleNavigate = (navigateTo) => {
+    console.log(navigateTo)
+    onClose()
+  }
+
   return (
-    <div>
+    <>
       <div className='menu_bar' onClick={showDrawer}>
         <FaBars style={{ color: '#fff' }} size={22} />
       </div>
-      <Drawer title='Basic Drawer' onClose={onClose} open={open} style={{ width: '300px' }}>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+      <Drawer title='Basic Drawer' onClose={onClose} open={open} className='menu_drawer'>
+        <div className='menu_drawer_body'>
+          <div className='close_button'>
+            <IoClose style={{ color: '#fff', cursor: 'pointer' }} size={22} onClick={onClose} />
+          </div>
+          <div className='company_logo'>
+            <img src={SamLogo} alt='Logo' />
+          </div>
+          <div className='menu_item_box'>
+            <div className='menu_item' onClick={() => handleNavigate('/')}>
+              <p>Football</p>
+            </div>
+            <div className='menu_item' onClick={() => handleNavigate('/')}>
+              <p>Baseball</p>
+            </div>
+            <div className='menu_item' onClick={() => handleNavigate('/')}>
+              <p>Hockey</p>
+            </div>
+            <div className='menu_item' onClick={() => handleNavigate('/')}>
+              <p>US Football</p>
+            </div>
+            <div className='menu_item' onClick={() => handleNavigate('/')}>
+              <p>College Football</p>
+            </div>
+            <div className='menu_item' onClick={() => handleNavigate('/')}>
+              <p>Scouting</p>
+            </div>
+            <div className='menu_item' onClick={() => handleNavigate('/')}>
+              <p>Login</p>
+            </div>
+            <div className='menu_item' onClick={() => handleNavigate('/')}>
+              <p>Signup</p>
+            </div>
+          </div>
+        </div>
       </Drawer>
-    </div>
+    </>
   )
 }
 
